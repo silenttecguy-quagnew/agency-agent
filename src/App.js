70d6d10ea01e7897a54d3f007aaf745eff160b77
silenttecguy-quagnew <silenttecguy@gmail.com>
2026-03-18 10:48:50 +0800

    Create src/App.js for agency-agent dashboard

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Agency Agent Dashboard</h1>
+            {/* Add your dashboard components here */}
+        </div>
+    );
+};
+
+export default Dashboard;
